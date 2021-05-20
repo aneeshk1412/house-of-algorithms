1d2f33b5338308e37c24274bee71d6627ff2e336
Aneesh Shetty <170040022iitbkai@gmail.com>
2021-05-20 18:35:09 +0530

    adds simpler and optimized function

--- shortest-path-faster-algorithm.cpp
+++ shortest-path-faster-algorithm.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 using ll = long long;
 
+inline void remove_element_from_average(double &avg, ll n, double e) {
+	avg = (1.0 * (n + 1) / (n + n==0)) * avg - (e / (n + n==0));
+}
+
+inline void add_element_to_average(double &avg, ll n, double e) {
+	avg = (1.0 * (n-1) / n) * avg - (e / n);
+}
+
+
 void
-shortest_path_faster(
+shortest_path_faster_simple(
 	vector<vector<pair<ll, double>>>& adj,
 	ll s, vector<double>& dist) {
 	
@@ -30,6 +39,50 @@ shortest_path_faster(
 					if (dist[Q.back()] < dist[Q.front()]) {
 						ll x=Q.back(); Q.pop_back();
 						Q.push_front(x);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+void
+shortest_path_faster(
+	vector<vector<pair<ll, double>>>& adj,
+	ll s, vector<double>& dist) {
+	
+	for (auto& d: dist)
+		d = numeric_limits<double>::max();
+
+	dist[s]=0.0;
+
+	deque<ll> Q;
+	vector<bool> inQ(dist.size(), false);
+	Q.push_back(s);
+	inQ[s]=true;
+	double avg=dist[s];
+
+	while (!Q.empty()) {
+		ll u=Q.front(); Q.pop_front(); inQ[u]=false;
+		remove_element_from_average(avg, Q.size(), dist[u]);
+
+		for (auto& [v, w]: adj[u]) {
+			if (dist[u] + w < dist[v]) {
+				dist[v] = dist[u] + w;
+				if (!inQ[v]) {
+					Q.push_back(v);
+					inQ[v]=true;
+					add_element_to_average(avg, Q.size(), dist[v]);
+
+					if (dist[Q.back()] < dist[Q.front()]) {
+						ll x=Q.back(); Q.pop_back();
+						Q.push_front(x);
+					}
+
+					while (dist[Q.front()] > avg) {
+						ll x=Q.front(); Q.pop_front();
+						Q.push_back(x);
 					}
 				}
 			}
