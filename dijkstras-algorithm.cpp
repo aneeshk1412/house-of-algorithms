ba7d36450d1da8131209f7a89deea2de5798619d
Aneesh Shetty <170040022iitbkai@gmail.com>
2021-05-20 07:08:29 +0530

    adds priority queue based implementation of dijkstra's algorithm

---
+++ dijkstras-algorithm.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void
+dijkstra_sssp(
+	vector<vector<pair<ll, double>>>& adj,
+	ll s, vector<double>& dist) {
+
+	vector<bool> vis(dist.size(), false);
+	for (auto& d: dist)
+		d = numeric_limits<double>::max();
+
+	dist[s]=0.0;
+
+	priority_queue<pair<double, ll>,
+	vector<pair<double, ll>>,
+	greater<pair<double, ll>>> q;
+
+	q.emplace(dist[s], s);
+
+	while (!q.empty()) {
+		ll u=q.top().second; q.pop();
+		if (vis[u]) continue;
+		vis[u]=true;
+
+		for (auto& [v, w]: adj[u]) {
+			if (dist[u] + w < dist[v]) {
+				dist[v] = dist[u] + w;
+				q.emplace(dist[v], v);
+			}
+		}
+	}
+
+	for (auto d: dist) {
+		cout << d << " ";
+	}
+	cout << "\n";
+}
+
+int
+main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
